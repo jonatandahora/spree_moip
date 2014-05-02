56f0dba65fa9920250d3433a25e668501dd82880
jonatan <jonatandahora@gmail.com>
2014-05-02 18:49:21 -0300

    remove validations

--- app/models/address_decorator.rb
+++ app/models/address_decorator.rb
@@ -1,18 +1,18 @@
-Spree::Address.class_eval do
+#Spree::Address.class_eval do
 
-  def create
-    Address.create(address_params)
-  end
+ # def create
+  #  Address.create(address_params)
+  #end
 
-  private
+  #private
 
-  def address_params
-    params.permit(:address_number, :district)
-  end
+  #def address_params
+   # params.permit(:address_number, :district)
+  #end
 
 
 
-  validates :address_number, :district, presence: true
-  validates :zipcode,:format => { :with => /^(\d){5}-(\d){3}$/, multiline:true }
-  validates :phone, :format => { :with => /^\((\d){2}\)(\d){4}-(\d){4,5}$/ , multiline:true}
-end
+  #validates :address_number, :district, presence: true
+  #validates :zipcode,:format => { :with => /^(\d){5}-(\d){3}$/, multiline:true }
+  #validates :phone, :format => { :with => /^\((\d){2}\)(\d){4}-(\d){4,5}$/ , multiline:true}
+#end
