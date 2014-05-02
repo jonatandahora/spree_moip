698499ad55dcebfbda20c2624157573dee5c62f3
jonatan <jonatandahora@gmail.com>
2014-05-02 19:05:35 -0300

    remove validations

--- db/migrate/20120815131046_add_address_fields_to_spree_address.rb
+++
@@ -1,6 +0,0 @@
-class AddAddressFieldsToSpreeAddress < ActiveRecord::Migration
-  def change
-  #  add_column :spree_addresses, :address_number, :string
-  #  add_column :spree_addresses, :district, :string
-  end
-end
