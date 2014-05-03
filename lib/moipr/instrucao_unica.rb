a957193639950d7ebe2df1198e231f2cc0294f50
jonatan <jonatandahora@gmail.com>
2014-05-02 21:43:14 -0300

    resolve error 401

--- lib/moipr/instrucao_unica.rb
+++ lib/moipr/instrucao_unica.rb
@@ -1,26 +1,27 @@
-require 'base64'
 module Moipr
   class InstrucaoUnica
-    
     def initialize(xml)
       @xml = xml
-      
     end
 
     def request
-      req = RestClient::Request.new
-      req.basic_auth(Moipr.configuration.secret_token, Moipr.configuration.secret_key)
+     # req = RestClient::Request.new(params)
+     # req.basic_auth Moipr.configuration.secret_token, Moipr.configuration.secret_key
       req.execute(params)
     end
+
     private
     def params
       {
         :method => :post,
         :url => Moipr.configuration.url,
         :payload => @xml.build,
+        :user => Moipr.configuration.secret_key,
+        :password => Moipr.configuration.secret_token,
         :headers => {
           :content_type => :xml,
-          :accept => :xml
+          :accept => :xml,
+	  :Authorization => Base64.encode64(:password +":"+ :user)
         }
       }
     end
